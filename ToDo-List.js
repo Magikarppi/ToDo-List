cdd5b35a0ba14a34ae68ee9a5252a30b62f76d88
Sami <33135976+Magikarppi@users.noreply.github.com>
2018-09-11 00:28:39 +0300

    update to ToDo-List.js

--- ToDo-List.js
+++ ToDo-List.js
@@ -3,25 +3,11 @@ Practical javascript project
 
 const toDoArr = {
   todos: [],
-  displayTodos: function(){
-    if (this.todos === undefined || this.todos.length < 1){
-      console.log('Your todo list is empty :(');
-    } else {
-    for (i=0; i < this.todos.length; i++){
-      if (this.todos[i].completed === true){
-        console.log('(x) ', this.todos[i].textTodo);
-      } else {
-        console.log('( ) ', this.todos[i].textTodo);
-      }
-    }
-    }
-  }, 
   addTodos: function(textTodo){
     this.todos.push({
       textTodo : textTodo,
       completed : false
-   });
-  //this.displayTodos();
+    });
   },
   changeTodos: function(todoText, pos){
     this.todos[pos].textTodo = todoText;
@@ -32,38 +18,94 @@ const toDoArr = {
   toggleCompleted: function(pos){
     const todo = this.todos[pos];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll: function(){
     // find out how many are completed
     var completedTodos = 0;
-    for (i=0; i < this.todos.length; i++){
-      if (this.todos[i].completed === true){
+    var todosLength = this.todos.length;
+    this.todos.forEach(function(element) {
+      if (element.completed === true){
         completedTodos++;
       }
-    }
+    })
     // case 1: if everything is true turn them to false
-    if (this.todos.length === completedTodos){
-      for (i=0; i < this.todos.length; i++){
-        this.todos[i].completed = false;
-      }
+    this.todos.forEach(function(element){
+      if (todosLength === completedTodos){
+        element.completed = false;
     // case 2: otherwise turn everything to true
-    } else { 
-        for (i=0; i < this.todos.length; i++){
-        this.todos[i].completed = true;
-        } 
-    }
-      this.displayTodos();
+      } else { 
+        element.completed = true;
+      }
+      })
   }
+};
+
+const handlers = {
+    addTodos: function(){
+        toDoArr.addTodos(document.getElementById("addTodoInput").value);
+        document.getElementById("addTodoInput").value = '';
+        viewers.displayTodos();
+    },
+    deleteTodos: function(position){
+      toDoArr.deleteTodos(position);
+      viewers.displayTodos();
+    },
+    changeTodos: function(){
+        const changeTodoText = document.getElementById("changeTodoText");
+        const changeTodoNumber = document.getElementById("changeTodoNumber");
+        toDoArr.changeTodos(changeTodoText.value, changeTodoNumber.valueAsNumber);
+        changeTodoText.value = '';
+        changeTodoNumber.value = null;
+        viewers.displayTodos();
+    },
+    toggleCompleted: function(){
+        const toggleCompletedNumber = document.getElementById("toggleCompleted");
+        toDoArr.toggleCompleted(toggleCompletedNumber.valueAsNumber);
+        toggleCompletedNumber.value = null;
+        viewers.displayTodos();
+    },
+    toggleAll: function(){
+        toDoArr.toggleAll();
+        viewers.displayTodos();
+    }
+};
+
+const viewers = {
+    displayTodos: function(){
+        const todoUl = document.querySelector('ul');
+        todoUl.innerHTML = '';
+        toDoArr.todos.forEach(function(element, position){
+            const todoLi = document.createElement('li');
+           
+            if(element.completed === true){
+                todoLi.textContent = '(x) ' + element.textTodo;
+            } else {
+                todoLi.textContent = '( ) ' + element.textTodo;
+            }
+
+            todoLi.id = position;
+            todoLi.appendChild(this.createDelButton());
+            todoUl.appendChild(todoLi);
+        }, this)
+    },
+    createDelButton: function(){
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'deleteButton';
+      return deleteButton;
+    },
+    setUpEventListeners: function(){
+      const todosUl = document.querySelector('ul');
+      todosUl.addEventListener('click', function(event){
+        const elementClicked = event.target;
+
+       if (elementClicked.className === 'deleteButton'){
+        handlers.deleteTodos(elementClicked.parentNode.id)
+        }
+      })
+    }
 }
+viewers.setUpEventListeners();
 
-const displayTodosButton = document.getElementById("displayTodosButton");
+    
 
-displayTodosButton.addEventListener('click', function(){
-    toDoArr.displayTodos();
-});
-const toggleAllButton = document.getElementById("toggleAllButton");
-
-toggleAllButton.addEventListener('click', function(){
-    toDoArr.toggleAll();
-});
